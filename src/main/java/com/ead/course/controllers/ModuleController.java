cb8d3ea26e6352bbbd16815f6660fb2418c182c0
Marcos Vassler <marcosvassoler@finchsolucoes.com.br>
2021-11-22 11:15:04 -0300

    Commit com as implementações do CRUD para a entidade Module.

---
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -0,0 +1,86 @@
+package com.ead.course.controllers;
+
+import com.ead.course.dtos.ModuleDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.ModuleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/modules")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ModuleController {
+
+    @Autowired
+    private ModuleService moduleService;
+
+    @Autowired
+    private CourseService courseService;
+
+    @PostMapping("/courses/{courseId}/modules")
+    public ResponseEntity<Object> saveModulo(@PathVariable(value = "courseId") UUID courseId,
+                                             @RequestBody @Valid ModuleDto moduleDto){
+        Optional<CourseModel> courseModelOptional = this.courseService.findById(courseId);
+        if(!courseModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
+        }
+        var moduleModel = new ModuleModel();
+        BeanUtils.copyProperties(moduleDto, moduleModel);
+        moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        moduleModel.setCourse(courseModelOptional.get());
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.moduleService.save(moduleModel));
+    }
+
+    @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> deleteMoudle(@PathVariable(value = "courseId") UUID courseId,
+                                               @PathVariable(value = "moduleId") UUID moduleId){
+        Optional<ModuleModel> moduleModelOptional = this.moduleService.findModuleIntoCourse(courseId, moduleId);
+        if(!moduleModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found For This Course.");
+        }
+        this.moduleService.delete(moduleModelOptional.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Module deleted sucessfully.");
+    }
+
+    @PutMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> updateModule(@PathVariable(value = "courseId") UUID courseId,
+                                               @PathVariable(value = "moduleId") UUID moduleId,
+                                               @RequestBody @Valid ModuleDto moduleDto){
+        Optional<ModuleModel> moduleModelOptional = this.moduleService.findModuleIntoCourse(courseId, moduleId);
+        if(!moduleModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found For This Course.");
+        }
+        var moduleModel = moduleModelOptional.get();
+        moduleModel.setTitle(moduleDto.getTitle());
+        moduleModel.setDescription(moduleDto.getDescripton());
+        return ResponseEntity.status(HttpStatus.OK).body(this.moduleService.save(moduleModel));
+    }
+
+    @GetMapping("/courses/{courseId}/modules")
+    public ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable(value = "courseId") UUID courseId){
+        return ResponseEntity.status(HttpStatus.OK).body(this.moduleService.findAllByCourse(courseId));
+    }
+
+    @GetMapping("/courses/{courseId}/modules/{moduleId}")
+    public ResponseEntity<Object> getOneModule(@PathVariable(value = "courseId") UUID courseId,
+                                               @PathVariable(value = "moduleId") UUID moduleId){
+        Optional<ModuleModel> moduleModelOptional = this.moduleService.findModuleIntoCourse(courseId, moduleId);
+        if(!moduleModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found For This Course.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(moduleModelOptional.get());
+    }
+
+}
--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ModuleServiceImpl implements ModuleService {
@@ -29,5 +31,25 @@ public class ModuleServiceImpl implements ModuleService {
         }
         this.moduleRepository.delete(moduleModel);
 
+    }
+
+    @Override
+    public ModuleModel save(ModuleModel moduleModel) {
+        return this.moduleRepository.save(moduleModel);
+    }
+
+    @Override
+    public Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId) {
+        return moduleRepository.findModuleIntoCourse(courseId, moduleId);
+    }
+
+    @Override
+    public List<ModuleModel> findAll() {
+        return this.moduleRepository.findAll();
+    }
+
+    @Override
+    public List<ModuleModel> findAllByCourse(UUID courseId) {
+        return this.moduleRepository.findAllModulesIntoCourse(courseId);
     }
 }
